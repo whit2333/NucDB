76a6699044ec6e9927838cfb412aededdc1df3bf
Whitney Armstrong <whit@temple.edu>
2012-04-02 13:29:33 -0400

    Added paper and calculation

---
+++ include/NucDBCalculation.h
@@ -0,0 +1,75 @@
+#ifndef NucDBCalculation_HH
+#define NucDBCalculation_HH 1
+#include "TNamed.h"
+#include "TList.h"
+#include <iostream>
+#include "NucDBMeasurement.h"
+#include "NucDBDataPoint.h"
+#include "TGraphErrors.h"
+#include "TCanvas.h"
+
+/** Base class for a calculation object.
+ * 
+ * A calculation then includes all measured quantities as measurements.
+ * The name should be the name used to query the database and the title
+ * is used for display purposes.
+ */
+class NucDBCalculation : public TNamed {
+public:
+   NucDBCalculation(const char * name ="unknowncalc",const char * title="unknown") : TNamed(name,title) {
+      fMeasurements.Clear();
+   }
+   ~NucDBCalculation() { }
+
+   NucDBMeasurement * GetMeasurement(const char * name) {
+      NucDBMeasurement * meas = 0;
+      for(int i = 0;i<fMeasurements.GetEntries();i++) {
+         if( !strcmp(((NucDBMeasurement *)fMeasurements.At(i))->GetName(),name) ) {
+            meas = (NucDBMeasurement *)fMeasurements.At(i) ;
+            break;
+         }
+      }
+      if(!meas) std::cout << " Calculation named " << name << " NOT FOUND!\n";
+      return(meas);
+   }
+   
+protected: 
+   TList fMeasurements;
+   TList fFunctions;
+   
+public:
+   TString fPaper;
+   TString fReference;   
+
+public:
+   void AddMeasurement(NucDBMeasurement* meas){
+      if(meas) meas->fExperiment = GetName();
+      if(meas) fMeasurements.Add(meas);
+   }
+
+   void Print(){
+      std::cout << "===================================\n";
+      std::cout << "   " << GetName() << "\n";
+      std::cout << "===================================\n";
+      std::cout << "  title = " << GetTitle() << "\n";
+      std::cout << "+++++ Calculations +++++\n";
+      for(int i =0; i<fMeasurements.GetEntries();i++)
+          ((NucDBMeasurement*)fMeasurements.At(i))->Print();
+   }
+
+   void PlotMeasurements(const char * var = "x") {
+      TCanvas * c1 = new TCanvas( Form("%splots",GetName()),Form("%s plots",GetTitle() ) );
+      c1->Divide(2,fMeasurements.GetEntries()/2);
+      for(int i =0; i<fMeasurements.GetEntries();i++) {
+          c1->cd(i+1);
+          ((NucDBMeasurement*)fMeasurements.At(i))->BuildGraph(var);
+          ((NucDBMeasurement*)fMeasurements.At(i))->fGraph->Draw("ap");
+      }
+   }
+
+ClassDef(NucDBCalculation,1)
+};
+
+
+
+#endif
---
+++ include/NucDBPaper.h
@@ -0,0 +1,78 @@
+#ifndef NucDBPapers_HH
+#define NucDBPapers_HH
+#include "TNamed.h"
+#include "TList.h"
+#include <iostream>
+#include "NucDBCalculation.h"
+#include "NucDBDataPoint.h"
+#include "TGraphErrors.h"
+#include "TCanvas.h"
+
+/** Representation of a published or unpublished Paper.
+ * 
+ *  
+ */
+class NucDBPaper : public TNamed {
+public:
+   NucDBPaper(const char * name ="apaper",const char * title="unknown"):TNamed(name,title) {
+      fCalculations.Clear();
+   }
+   ~NucDBPaper(){}
+
+   NucDBCalculation * GetCalculation(const char * name) {
+      NucDBCalculation * meas = 0;
+      for(int i = 0;i<fCalculations.GetEntries();i++) {
+         if( !strcmp(((NucDBCalculation *)fCalculations.At(i))->GetName(),name) ) {
+            meas = (NucDBCalculation *)fCalculations.At(i) ;
+            break;
+         }
+      }
+      if(!meas) std::cout << " Calculation named " << name << " NOT FOUND!\n";
+      return(meas);
+   }
+
+   const char * GetReference(){ return( fReference.Data() ); }
+
+protected: 
+   TList fCalculations;
+   TString fReference;
+   TString fAuthor;
+   TString fijef;
+
+public:
+   void AddCalculation(NucDBCalculation* c){
+      if(c){
+	 c->fPaper = GetName();
+         c->fReference = GetReference();
+         fCalculations.Add(c);
+      }
+   }
+
+   void Print(){
+      std::cout << "===================================\n";
+      std::cout << "   " << GetName() << "\n";
+      std::cout << "===================================\n";
+      std::cout << "  title = " << GetTitle() << "\n";
+      std::cout << "+++++ Calculations +++++\n";
+      for(int i =0; i<fCalculations.GetEntries();i++)
+          ((NucDBCalculation*)fCalculations.At(i))->Print();
+   }
+
+
+//    void PlotCl(const char * var = "x"){
+//       TCanvas * c1 = new TCanvas( Form("%splots",GetName()),Form("%s plots",GetTitle() ) );
+//       c1->Divide(2,fCalculations.GetEntries()/2);
+//       for(int i =0; i<fCalculations.GetEntries();i++) {
+//           c1->cd(i+1);
+//           ((NucDBPaper*)fCalculations.At(i))->BuildGraph(var);
+//           ((NucDBPaper*)fCalculations.At(i))->fGraph->Draw("ap");
+//       }
+//    }
+
+ClassDef(NucDBPaper,1)
+};
+
+
+
+
+#endif
