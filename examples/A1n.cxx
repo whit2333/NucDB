84ea7af0c2f553d13f87125def304fffd2f485dc
Whitney Armstrong <whit@temple.edu>
2012-10-17 13:59:14 -0400

    Added A1n script

--- examples/A1n.cxx
+++ examples/A1n.cxx
@@ -1,4 +1,4 @@
-Int_t A1p()
+Int_t A1n()
 {
 
    gSystem->Load("libNucDB");
@@ -9,7 +9,7 @@ Int_t A1p()
    TLegend * leg = new TLegend(0.1, 0.7, 0.48, 0.9);
    leg->SetHeader("The Legend Title");
 
-   TList * measurementsList = manager->GetMeasurements("A1p");
+   TList * measurementsList = manager->GetMeasurements("A1n");
    for (int i = 0; i < measurementsList->GetEntries(); i++) {
       NucDBMeasurement * g1p = (NucDBMeasurement*)measurementsList->At(i);
       TGraphErrors * graph = g1p->BuildGraph("x");
@@ -20,7 +20,7 @@ Int_t A1p()
 
    leg->Draw();
 
-   c->SaveAs("examples/A1p.png");
+   c->SaveAs("examples/A1n.png");
 
    return(0);
 }
