6fd7bd83b6c579a3ad129a5213015aded304f501
Whitney Armstrong <whit@temple.edu>
2013-03-21 17:32:38 -0400

    Added total cross section data and extractor for E94110

--- experiments/JLAB-E94110_NucDB.py
+++ experiments/JLAB-E94110_NucDB.py
@@ -4,6 +4,79 @@ from ROOT import NucDBManager,NucDBExperiment,NucDBMeasurement,NucDBDiscreteVari
 from NucDBExtractors import *
 import os
 
+class E94110FullSigmaExtractor(NucDBRawDataExtractor) :
+   #Full data extractor.Format :
+   # E        EP       Theta       Q^2           W^2       Nu       Epsilon    Gamma         x        Sigma         Uncertainty
+   #
+    def __init__(self):
+        NucDBRawDataExtractor.__init__(self)
+        self.iValueRow=9
+        self.istatErr=10
+
+    def ParseLine(self):
+        """ See input file for column structures
+        """
+        iEbeam=0
+        iEprime=1
+        iTheta=2
+        iQsq=3
+        iW2=4
+        iNu=5
+        iEpsilon=6
+        iGamma=7
+        ixbjorken=8
+        print self.currentline
+        values = self.currentline.split()
+        self.rowcut.currentValue=int(0) # does nothign
+        Eb = self.fCurrentDataPoint.GetBinVariable('E')
+        Eb.SetBinValueSize(float(values[iEbeam]),0.001)
+        Eb.Print()
+        Ep = self.fCurrentDataPoint.GetBinVariable('Eprime')
+        Ep.SetBinValueSize(float(values[iEprime]),0.001)
+        Ep.Print()
+        th = self.fCurrentDataPoint.GetBinVariable('theta')
+        th.SetBinValueSize(float(values[iTheta]),0.001)
+        th.Print()
+        nu = self.fCurrentDataPoint.GetBinVariable('nu')
+        nu.SetBinValueSize(float(values[iNu]),0.001)
+        nu.Print()
+        eps = self.fCurrentDataPoint.GetBinVariable('epsilon')
+        eps.SetBinValueSize(float(values[iEpsilon]),0.001)
+        eps.Print()
+        ga = self.fCurrentDataPoint.GetBinVariable('Gamma')
+        ga.SetBinValueSize(float(values[iGamma]),0.001)
+        ga.Print()
+        x = self.fCurrentDataPoint.GetBinVariable('x')
+        x.SetBinValueSize(float(values[ixbjorken]),0.001)
+        x.Print()
+        Qsq = self.fCurrentDataPoint.GetBinVariable("Qsquared")
+        Qsq.SetBinValueSize(float(values[iQsq]),0.001)
+        Qsq.Print()
+        W2 = self.fCurrentDataPoint.GetBinVariable("W2")
+        W2.SetBinValueSize(float(values[iW2]),0.001)
+        W2.Print()
+        self.fCurrentDataPoint.SetValue(float(values[self.iValueRow]))
+        self.fCurrentDataPoint.GetStatError().SetError(float(values[self.istatErr]))
+        self.fCurrentDataPoint.GetSystError().SetError(0)
+        #
+        W = self.fCurrentDataPoint.GetDependentVariable("W")
+        if not W :
+            W   = NucDBInvariantMassDV()
+            self.fCurrentDataPoint.AddDependentVariable(W)
+        if W :
+            W.SetVariable(0,x)
+            W.SetVariable(1,Qsq)
+        #nu = self.fCurrentDataPoint.GetDependentVariable("nu")
+        #if not nu :
+        #    nu   = NucDBPhotonEnergyDV()
+        #    self.fCurrentDataPoint.AddDependentVariable(nu)
+        #if nu :
+        #    nu.SetVariable(0,x)
+        #    nu.SetVariable(1,Qsq)
+        #self.fCurrentDataPoint.CalculateDependentVariables()
+        #
+        #self.fCurrentDataPoint.Print()
+
 class E94110SigmaExtractor(NucDBRawDataExtractor) :
     def __init__(self):
         NucDBRawDataExtractor.__init__(self)
@@ -121,7 +194,42 @@ if __name__ == "__main__":
     experiment = manager.GetExperiment("JLAB-E94110")
     if not experiment :
         experiment = NucDBExperiment("JLAB-E94110","JLAB-E94110")
-    
+
+    # sigma full 
+    sigma = experiment.GetMeasurement("sigma")
+    if not sigma :
+        sigma = NucDBMeasurement("sigma","#sigma^{p}")
+        experiment.AddMeasurement(sigma)
+    sigma.ClearDataPoints()
+    sigma.SetColor(4019)
+    sigmaExtractor = E94110FullSigmaExtractor()
+    sigmaExtractor.SetMeasurement(sigma)
+    sigmaExtractor.SetInputFile("experiments/JLAB-E94110/cs_94_sys.dat")
+    sigmaExtractor.linestoskip=1
+    sigmaExtractor.NumberOfLines=1273-1
+    Ebeam    = NucDBBinnedVariable("E","E")
+    Eprime   = NucDBBinnedVariable("Eprime","E'")
+    theta    = NucDBBinnedVariable("theta","#theta")
+    nu       = NucDBBinnedVariable("nu","#nu")
+    Gamma    = NucDBBinnedVariable("Gamma","#Gamma")
+    epsilon  = NucDBBinnedVariable("epsilon","#epsilon")
+    Xbjorken = NucDBBinnedVariable("x","x")
+    W2       = NucDBBinnedVariable("W2","W2")
+    Qsq      = NucDBBinnedVariable("Qsquared","Q^{2}")
+    sigmaExtractor.fCurrentDataPoint.AddBinVariable(Ebeam)
+    sigmaExtractor.fCurrentDataPoint.AddBinVariable(Eprime)
+    sigmaExtractor.fCurrentDataPoint.AddBinVariable(theta)
+    sigmaExtractor.fCurrentDataPoint.AddBinVariable(nu)
+    sigmaExtractor.fCurrentDataPoint.AddBinVariable(Gamma)
+    sigmaExtractor.fCurrentDataPoint.AddBinVariable(epsilon)
+    sigmaExtractor.fCurrentDataPoint.AddBinVariable(Xbjorken)
+    sigmaExtractor.fCurrentDataPoint.AddBinVariable(Qsq)
+    sigmaExtractor.fCurrentDataPoint.AddBinVariable(W2)
+    sigmaExtractor.Initialize()
+    sigmaExtractor.ExtractAllValues()
+    sigma.BuildGraph()
+
+   
     # sigma_L 
     sigmaL = experiment.GetMeasurement("sigma_L")
     if not sigmaL :
