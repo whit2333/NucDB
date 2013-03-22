3ae0c1bb7c995926935d5f41c3e2dab4664be1f9
Whitney Armstrong <whit@temple.edu>
2013-03-21 23:56:36 -0400

    Improved the extractor a bit. Need to fix existing extrators.

--- python/NucDBExtractors.py
+++ python/NucDBExtractors.py
@@ -13,7 +13,7 @@ from ROOT import NucDBManager,NucDBExperiment,NucDBMeasurement,NucDBDataPoint,Nu
 import copy
 
 """ @package NucDBRawDataExtractor
-    Used to extract experimental results from a raw data text file
+    Used to extract experimental results from a raw data text file.
 
     Implementation is specific to raw text file structure.
 """
@@ -34,48 +34,76 @@ class RowCut:
         else :
            return 0
 
+
+
 class NucDBRawDataExtractor:
-    """ ABC used to extract experimental results from a raw data text file
+    """ ABC for extracting experimental results from data text files.
         Implementation is specific to raw text file structure.
+        ::ParseLine needs to be implemented for each measurement.
+        The order of things is: 
+        ::__init_
+        ::SetMeasurement
+        ::SetInputFile
+        ::Initialize
+        ::ExtractAllValues
     """
     def __init__(self):
-        self.linestoskip=0
-        self.currentline=0
-        self.inputfile=0
-        self.inputfilename=0
-        self.fMeasurement=0
-        self.rowcut = RowCut()
+        self.linestoskip       = 0
+        self.currentline       = 0
+        self._line_number      = 0
+        self.inputfile         = 0
+        self.inputfilename     = 0
+        self.fMeasurement      = 0
+        self.NumberOfLines     = 0
+        self.rowcut            = RowCut()
         self.fCurrentDataPoint = NucDBDataPoint()
         self.fCurrentDataPoint.SetName("A")
         #self.fCurrentDataPoint.fDimension=2
 
     def ParseLine(self):
-        """ Specific to file structure. Should be reimplemented/modified for every measurment
-        """
+        """ Specific to file structure. Should be reimplemented/modified for every extractor.""" 
         values = self.currentline.split()
         #for i in range(9):
             #print str(i) + " = " + str(values[i])
 
-    def SetInputFile(self,filename):
+    def SetInputFile(self,filename,nskip=0,nread=0):
+        """Set the input file name. 
+           Optionally the number of header lines to skip and/or total number to read.
+        """
         self.inputfilename = filename
-        self.inputfile = open(self.inputfilename, "r")
+        self.linestoskip   = nskip
+        self.NInputLines   = nread
+        self.NumberOfLines = self.FileLen(filename)
+        if not nread:
+            self.NInputLines = self.NumberOfLines - nskip
+        self.inputfile     = open(self.inputfilename, "r")
+
+    def FileLen(self,fname):
+        with open(fname) as f:
+            for i, l in enumerate(f):
+                pass
+            return i + 1
 
     def SetMeasurement(self, meas):
         self.fMeasurement = meas
 
     def Initialize(self):
-        skip = self.linestoskip -1
+        "  "
+        skip = self.linestoskip 
         for i in range(skip):
             self.currentline = self.inputfile.readline() # keep it one line ahead of searchfile
+            _line_number     = _line_number + 1
             #print self.currentline
-        print "done"
+        print "Initialization Complete"
 
     def GetNextLine(self):
         self.currentline = self.inputfile.readline()
+        _line_number     = _line_number + 1
 
     def ExtractAllValues(self):
         for self.currentline in self.inputfile:
             self.GetNextValue()
+        print "Extraction Complete"
 
     def GetNextValue(self):
         if not self.fMeasurement :
@@ -84,3 +112,6 @@ class NucDBRawDataExtractor:
         if self.rowcut.IsGoodRow():
             self.fCurrentDataPoint.SetName("p"+str(self.fMeasurement.GetNDataPoints()));
             self.fMeasurement.AddDataPoint(copy.deepcopy(self.fCurrentDataPoint))
+
+
+
