c8421683543365e914f6de77b40d2752d22a7e1b
Whitney Armstrong <whit@temple.ed>
2013-12-16 00:12:46 -0500

    Adapting for root6. rootcling does not produce a header for the dictionary files

--- build/Functions.mk
+++ build/Functions.mk
@@ -15,13 +15,15 @@ FunctionsLibHead := $(FunctionsLibObjs:lib/%.o=$(incdir)/%.h)
 $(FunctionsLibObjs) : $(builddir)/%.o : %.h
 	$(CXX) $(CXXFLAGS) -c -o $@  $(@:$(builddir)/%.o=$(srcdir)/%.C)
 
-$(srcdir)/$(FunctionsLibName)Dict.cxx $(srcdir)/$(FunctionsLibName)Dict.h : $(FunctionsLibHead)\
-       $(incdir)/$(FunctionsLibName)_LinkDef.h 
-	rootcint -f $(srcdir)/$(FunctionsLibName)Dict.cxx $(RCINTFLAGS)  \
+#$(srcdir)/$(FunctionsLibName)Dict.cxx $(srcdir)/$(FunctionsLibName)Dict.h : $(FunctionsLibHead)\
+
+$(srcdir)/$(FunctionsLibName)Dict.cxx : $(FunctionsLibHead) $(incdir)/$(FunctionsLibName)_LinkDef.h 
+	rootcling -f $(srcdir)/$(FunctionsLibName)Dict.cxx $(RCINTFLAGS)  \
 	$(FunctionsLibHead)   $(incdir)/$(FunctionsLibName)_LinkDef.h 
-	mv $(srcdir)/$(FunctionsLibName)Dict.h $(incdir)/.
+	#mv $(srcdir)/$(FunctionsLibName)Dict.h $(incdir)/.
 #_____________________________________________________________________________/
-$(builddir)/$(FunctionsLibName)Dict.o : $(FunctionsLibName)Dict.cxx $(incdir)/$(FunctionsLibName)Dict.h
+#$(builddir)/$(FunctionsLibName)Dict.o : $(FunctionsLibName)Dict.cxx $(incdir)/$(FunctionsLibName)Dict.h
+$(builddir)/$(FunctionsLibName)Dict.o : $(FunctionsLibName)Dict.cxx 
 	$(CXX) $(CXXFLAGS) -c -fPIC -o $@  $(srcdir)/$(FunctionsLibName)Dict.cxx 
 #_____________________________________________________________________________/
 $(FunctionsLibName) : $(FunctionsLibObjs) $(builddir)/$(FunctionsLibName)Dict.o 
--- build/General.mk
+++ build/General.mk
@@ -25,13 +25,16 @@ GeneralLibHead := $(GeneralLibObjs:lib/%.o=$(incdir)/%.h)
 $(GeneralLibObjs) : $(builddir)/%.o : %.h
 	$(CXX) $(CXXFLAGS) -c -o $@  $(@:$(builddir)/%.o=$(srcdir)/%.C)
 
-$(srcdir)/$(GeneralLibName)Dict.cxx $(srcdir)/$(GeneralLibName)Dict.h : $(GeneralLibHead)\
-       $(incdir)/$(GeneralLibName)_LinkDef.h 
-	rootcint -f $(srcdir)/$(GeneralLibName)Dict.cxx $(RCINTFLAGS)  \
+#$(srcdir)/$(GeneralLibName)Dict.cxx $(srcdir)/$(GeneralLibName)Dict.h : $(GeneralLibHead)\
+
+$(srcdir)/$(GeneralLibName)Dict.cxx : $(GeneralLibHead) $(incdir)/$(GeneralLibName)_LinkDef.h 
+	rootcling -f $(srcdir)/$(GeneralLibName)Dict.cxx $(RCINTFLAGS)  \
 	$(GeneralLibHead)   $(incdir)/$(GeneralLibName)_LinkDef.h 
-	mv $(srcdir)/$(GeneralLibName)Dict.h $(incdir)/.
+	#mv $(srcdir)/$(GeneralLibName)Dict.h $(incdir)/.
 #_____________________________________________________________________________/
-$(builddir)/$(GeneralLibName)Dict.o : $(GeneralLibName)Dict.cxx $(incdir)/$(GeneralLibName)Dict.h
+#$(builddir)/$(GeneralLibName)Dict.o : $(GeneralLibName)Dict.cxx $(incdir)/$(GeneralLibName)Dict.h
+
+$(builddir)/$(GeneralLibName)Dict.o : $(GeneralLibName)Dict.cxx 
 	$(CXX) $(CXXFLAGS) -c -fPIC -o $@  $(srcdir)/$(GeneralLibName)Dict.cxx 
 #_____________________________________________________________________________/
 $(GeneralLibName) : $(GeneralLibObjs) $(builddir)/$(GeneralLibName)Dict.o $(FortranLibObjs)
