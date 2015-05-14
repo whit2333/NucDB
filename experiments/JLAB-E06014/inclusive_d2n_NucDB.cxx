f08aede8920f303ce5dce495810c2361c586f045
Whitney Armstrong <whit@temple.edu>
2015-05-14 16:01:42 -0500

    modified:   experiments/JLAB-E06014/inclusive_d2n_NucDB.cxx 	modified:   experiments/SLAC-WISER/inclusive_pion_NucDB.cxx 	modified:   experiments/SLAC-WISER/wiser/table_IV_proton.txt

--- experiments/JLAB-E06014/inclusive_d2n_NucDB.cxx
+++ experiments/JLAB-E06014/inclusive_d2n_NucDB.cxx
@@ -15,7 +15,7 @@ Int_t inclusive_d2n_NucDB(){
    NucDBBinnedVariable    theta("theta","#theta_{#pi}",45.0,0.25); // unknown bin size
    NucDBBinnedVariable    T(  "T","T_{#pi}"); // unknown bin size
    NucDBBinnedVariable    Ppi("P","p_{#pi}");
-   NucDBBinnedVariable    PTpi("P_T","p_{T}");
+   NucDBBinnedVariable    PTpi("PT","p_{T}");
    NucDBBinnedVariable    Epi("E","E_{#pi}");
 
    NucDBDataPoint   * apoint = 0 ;
