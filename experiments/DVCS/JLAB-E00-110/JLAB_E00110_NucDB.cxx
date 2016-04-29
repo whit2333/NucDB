fc26d03ca1509b50a6ccf1e938baa03c92f477d8
Whitney Armstrong <warmstrong@anl.gov>
2016-04-29 10:17:47 -0500

    modified:   JLAB_E00110_NucDB.cxx 	new file:   table_IX.dat 	modified:   table_IX.txt 	new file:   table_XI.dat 	new file:   table_XII.dat 	new file:   table_XIII.dat 	new file:   table_XIV.dat 	new file:   table_XV.dat

--- experiments/DVCS/JLAB-E00-110/JLAB_E00110_NucDB.cxx
+++ experiments/DVCS/JLAB-E00-110/JLAB_E00110_NucDB.cxx
@@ -1,16 +1,6 @@
-void JLAB_E00110_NucDB()
+
+void add_DVCS_data(const char * fname, NucDBMeasurement * meas)
 {
-   NucDBManager * dbman = NucDBManager::GetManager(1);
-
-   auto experiment = dbman->GetExperiment("JLAB-E00110");
-   if( !experiment ){
-      experiment = new NucDBExperiment("JLAB-E00110","JLAB E00110");
-
-   }
-   auto meas = new NucDBMeasurement("sigma_p","d#sigma/dxdtd#phi dQ^{2}");
-   meas->ClearDataPoints();
-   experiment->AddMeasurement(meas);
-
    NucDBBinnedVariable  x_var(  "x","x");
    NucDBBinnedVariable  t_var(  "t","t");
    NucDBBinnedVariable  Q2_var( "Qsquared","Q^{2}");
@@ -21,7 +11,6 @@ void JLAB_E00110_NucDB()
    point.AddBinVariable(  &x_var);
    point.AddBinVariable(  &t_var);
    point.AddBinVariable( &Q2_var);
-
    point.AddBinVariable(&phi_var);
 
    const int Ncols = 5;
@@ -35,129 +24,52 @@ void JLAB_E00110_NucDB()
 
    std::string line;
    std::stringstream iss(line);
-   // ----------------------------------------------------------------
-   std::ifstream afile("experiments/DVCS/JLAB-E00-110/table_VII.dat");
-
-   std::getline(afile, line);
-   iss = std::stringstream(line);
-   for(int i = 0; i< Ncols; i++ ) {
-      iss >> x[i];
-      //std::cout << "x = " << x[i] << std::endl;
-   }
-   std::getline(afile, line);
-   iss = std::stringstream(line);
-   for(int i = 0; i< Ncols; i++ ) {
-      iss >> Q2[i];
-      //std::cout << "Q2= " << Q2[i] << std::endl;
-   }
-   std::getline(afile, line);
-   iss = std::stringstream(line);
-   for(int i = 0; i< Ncols; i++ ) {
-      iss >> t[i];
-      //std::cout << "t = " << t[i] << std::endl;
-   }
-
    double phi = 0;
-   while (std::getline(afile, line))
-   {
-      iss = std::stringstream(line);
-      std::cout << line << std::endl;
-
-      iss >> phi;
-      //std::cout << "phi = " << phi << std::endl;
-
-      double x1,x2,x3;
-      for(int i = 0; i< Ncols; i++ ) {
-         iss >> x1;
-         iss >> x2;
-         iss >> x3;
-         std::cout << x1 <<std::endl;
-         std::cout << x2 <<std::endl;
-         std::cout << x3 <<std::endl;
-         sig[i] = x1;
-         eStat[i] = x2;
-         eSys1[i] = x3;
-         std::cout << "sig = " << sig[i] << " " << eStat[i]  << " " << eSys1[i] << std::endl;;
-      }
-
-      std::getline(afile, line);
-      iss = std::stringstream(line);
-      std::cout << line << std::endl;
-      for(int i = 0; i< Ncols; i++ ) {
-         iss >> eSys2[i];
-         //std::cout << "sys2= " << eSys2[i] << std::endl;;
-      }
-      for(int i = 0; i< Ncols; i++ ) {
-
-         x_var.SetValueSize(   x[i], 0.001);
-         t_var.SetValueSize(   t[i], 0.001);
-         Q2_var.SetValueSize( Q2[i], 0.001);
-         phi_var.SetValueSize(  phi, 0.001);
-
-         point.SetValue( sig[i]  ); 
-         // Set the errors
-         point.SetStatError( eStat[i] ); 
-         point.SetSystError( NucDBErrorBar(eSys1[i],eSys2[i]) ); 
-         point.CalculateTotalError();
-         point.Print();
-         // copy the point and add it to the measurement
-         meas->AddDataPoint(new NucDBDataPoint(point));
-      }
-   }
-   afile.close();
-
    // ----------------------------------------------------------------
-   afile = std::ifstream("experiments/DVCS/JLAB-E00-110/table_VIII.dat");
+   //
+   afile = std::ifstream(fname);
 
    std::getline(afile, line);
    iss = std::stringstream(line);
    for(int i = 0; i< Ncols; i++ ) {
       iss >> x[i];
-      //std::cout << "x = " << x[i] << std::endl;
    }
    std::getline(afile, line);
    iss = std::stringstream(line);
    for(int i = 0; i< Ncols; i++ ) {
       iss >> Q2[i];
-      //std::cout << "Q2= " << Q2[i] << std::endl;
    }
    std::getline(afile, line);
    iss = std::stringstream(line);
    for(int i = 0; i< Ncols; i++ ) {
       iss >> t[i];
-      //std::cout << "t = " << t[i] << std::endl;
    }
 
    while (std::getline(afile, line))
    {
       iss = std::stringstream(line);
-      std::cout << line << std::endl;
-
       iss >> phi;
-      //std::cout << "phi = " << phi << std::endl;
 
+      int N_cols_read = 0;
       double x1,x2,x3;
       for(int i = 0; i< Ncols; i++ ) {
-         iss >> x1;
+         if(!(iss >> x1)) {break;}
          iss >> x2;
          iss >> x3;
-         std::cout << x1 <<std::endl;
-         std::cout << x2 <<std::endl;
-         std::cout << x3 <<std::endl;
          sig[i] = x1;
          eStat[i] = x2;
          eSys1[i] = x3;
-         std::cout << "sig = " << sig[i] << " " << eStat[i]  << " " << eSys1[i] << std::endl;;
+         N_cols_read++;
       }
 
       std::getline(afile, line);
       iss = std::stringstream(line);
-      std::cout << line << std::endl;
       for(int i = 0; i< Ncols; i++ ) {
-         iss >> eSys2[i];
-         //std::cout << "sys2= " << eSys2[i] << std::endl;;
+         if(!(iss >> eSys2[i])){
+            break;
+         }
       }
-      for(int i = 0; i< Ncols; i++ ) {
+      for(int i = 0; i< N_cols_read; i++ ) {
 
          x_var.SetValueSize(   x[i], 0.001);
          t_var.SetValueSize(   t[i], 0.001);
@@ -173,79 +85,52 @@ void JLAB_E00110_NucDB()
          // copy the point and add it to the measurement
          meas->AddDataPoint(new NucDBDataPoint(point));
       }
+      //std::cout << " cols : " << N_cols_read << std::endl;
    }
    afile.close();
+}
+
+void JLAB_E00110_NucDB()
+{
+   NucDBManager * dbman = NucDBManager::GetManager(1);
+
+   auto experiment = dbman->GetExperiment("JLAB-E00110");
+   if( !experiment ){
+      experiment = new NucDBExperiment("JLAB-E00110","JLAB E00110");
+
+   }
 
    // ----------------------------------------------------------------
-   afile = std::ifstream("experiments/DVCS/JLAB-E00-110/table_XII.dat");
-
-   std::getline(afile, line);
-   iss = std::stringstream(line);
-   for(int i = 0; i< Ncols; i++ ) {
-      iss >> x[i];
-      //std::cout << "x = " << x[i] << std::endl;
+   // unpolarized cross section
+   auto meas = experiment->GetMeasurement("sigma_p");
+   if( !meas ) {
+      meas = new NucDBMeasurement("sigma_p","d#sigma/dxdtd#phi dQ^{2}");
+      experiment->AddMeasurement(meas);
    }
-   std::getline(afile, line);
-   iss = std::stringstream(line);
-   for(int i = 0; i< Ncols; i++ ) {
-      iss >> Q2[i];
-      //std::cout << "Q2= " << Q2[i] << std::endl;
+   meas->ClearDataPoints();
+
+   add_DVCS_data("experiments/DVCS/JLAB-E00-110/table_VII.dat",meas);
+   add_DVCS_data("experiments/DVCS/JLAB-E00-110/table_VIII.dat",meas);
+   add_DVCS_data("experiments/DVCS/JLAB-E00-110/table_XII.dat",meas);
+   add_DVCS_data("experiments/DVCS/JLAB-E00-110/table_XIII.dat",meas);
+  
+   // ----------------------------------------------------------------
+   meas = experiment->GetMeasurement("delta_sigma_p");
+   if( !meas ) {
+      meas = new NucDBMeasurement("delta_sigma_p","#Deltad#sigma/dxdtd#phi dQ^{2}");
+      experiment->AddMeasurement(meas);
    }
-   std::getline(afile, line);
-   iss = std::stringstream(line);
-   for(int i = 0; i< Ncols; i++ ) {
-      iss >> t[i];
-      //std::cout << "t = " << t[i] << std::endl;
-   }
-
-   while (std::getline(afile, line))
-   {
-      iss = std::stringstream(line);
-      std::cout << line << std::endl;
-
-      iss >> phi;
-      //std::cout << "phi = " << phi << std::endl;
-
-      double x1,x2,x3;
-      for(int i = 0; i< Ncols; i++ ) {
-         iss >> x1;
-         iss >> x2;
-         iss >> x3;
-         std::cout << x1 <<std::endl;
-         std::cout << x2 <<std::endl;
-         std::cout << x3 <<std::endl;
-         sig[i] = x1;
-         eStat[i] = x2;
-         eSys1[i] = x3;
-         std::cout << "sig = " << sig[i] << " " << eStat[i]  << " " << eSys1[i] << std::endl;;
-      }
-
-      std::getline(afile, line);
-      iss = std::stringstream(line);
-      std::cout << line << std::endl;
-      for(int i = 0; i< Ncols; i++ ) {
-         iss >> eSys2[i];
-         //std::cout << "sys2= " << eSys2[i] << std::endl;;
-      }
-      for(int i = 0; i< Ncols; i++ ) {
-
-         x_var.SetValueSize(   x[i], 0.001);
-         t_var.SetValueSize(   t[i], 0.001);
-         Q2_var.SetValueSize( Q2[i], 0.001);
-         phi_var.SetValueSize(  phi, 0.001);
-
-         point.SetValue( sig[i]  ); 
-         // Set the errors
-         point.SetStatError( eStat[i] ); 
-         point.SetSystError( NucDBErrorBar(eSys1[i],eSys2[i]) ); 
-         point.CalculateTotalError();
-         point.Print();
-         // copy the point and add it to the measurement
-         meas->AddDataPoint(new NucDBDataPoint(point));
-      }
-   }
+   meas->ClearDataPoints();
+   add_DVCS_data("experiments/DVCS/JLAB-E00-110/table_IX.dat",meas);
+   add_DVCS_data("experiments/DVCS/JLAB-E00-110/table_X.dat",meas);
+   add_DVCS_data("experiments/DVCS/JLAB-E00-110/table_XI.dat",meas);
+   add_DVCS_data("experiments/DVCS/JLAB-E00-110/table_XIV.dat",meas);
+   
+   // ----------------------------------------------------------------
    auto gr = meas->BuildGraph("phi");
    gr->Draw("aep");
+
+   experiment->Print();
 
    dbman->SaveExperiment(experiment);
 }
