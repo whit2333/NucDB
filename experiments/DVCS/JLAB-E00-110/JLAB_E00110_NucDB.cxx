e4408d9142df247973fbe3418902a1f08949c13b
Whitney Armstrong <warmstrong@anl.gov>
2016-04-28 23:14:43 -0500

    modified:   JLAB_E00110_NucDB.cxx

--- experiments/DVCS/JLAB-E00-110/JLAB_E00110_NucDB.cxx
+++ experiments/DVCS/JLAB-E00-110/JLAB_E00110_NucDB.cxx
@@ -174,6 +174,76 @@ void JLAB_E00110_NucDB()
          meas->AddDataPoint(new NucDBDataPoint(point));
       }
    }
+   afile.close();
+
+   // ----------------------------------------------------------------
+   afile = std::ifstream("experiments/DVCS/JLAB-E00-110/table_XII.dat");
+
+   std::getline(afile, line);
+   iss = std::stringstream(line);
+   for(int i = 0; i< Ncols; i++ ) {
+      iss >> x[i];
+      //std::cout << "x = " << x[i] << std::endl;
+   }
+   std::getline(afile, line);
+   iss = std::stringstream(line);
+   for(int i = 0; i< Ncols; i++ ) {
+      iss >> Q2[i];
+      //std::cout << "Q2= " << Q2[i] << std::endl;
+   }
+   std::getline(afile, line);
+   iss = std::stringstream(line);
+   for(int i = 0; i< Ncols; i++ ) {
+      iss >> t[i];
+      //std::cout << "t = " << t[i] << std::endl;
+   }
+
+   while (std::getline(afile, line))
+   {
+      iss = std::stringstream(line);
+      std::cout << line << std::endl;
+
+      iss >> phi;
+      //std::cout << "phi = " << phi << std::endl;
+
+      double x1,x2,x3;
+      for(int i = 0; i< Ncols; i++ ) {
+         iss >> x1;
+         iss >> x2;
+         iss >> x3;
+         std::cout << x1 <<std::endl;
+         std::cout << x2 <<std::endl;
+         std::cout << x3 <<std::endl;
+         sig[i] = x1;
+         eStat[i] = x2;
+         eSys1[i] = x3;
+         std::cout << "sig = " << sig[i] << " " << eStat[i]  << " " << eSys1[i] << std::endl;;
+      }
+
+      std::getline(afile, line);
+      iss = std::stringstream(line);
+      std::cout << line << std::endl;
+      for(int i = 0; i< Ncols; i++ ) {
+         iss >> eSys2[i];
+         //std::cout << "sys2= " << eSys2[i] << std::endl;;
+      }
+      for(int i = 0; i< Ncols; i++ ) {
+
+         x_var.SetValueSize(   x[i], 0.001);
+         t_var.SetValueSize(   t[i], 0.001);
+         Q2_var.SetValueSize( Q2[i], 0.001);
+         phi_var.SetValueSize(  phi, 0.001);
+
+         point.SetValue( sig[i]  ); 
+         // Set the errors
+         point.SetStatError( eStat[i] ); 
+         point.SetSystError( NucDBErrorBar(eSys1[i],eSys2[i]) ); 
+         point.CalculateTotalError();
+         point.Print();
+         // copy the point and add it to the measurement
+         meas->AddDataPoint(new NucDBDataPoint(point));
+      }
+   }
    auto gr = meas->BuildGraph("phi");
    gr->Draw("aep");
 
